d83031ab1754fad367efe6e67b3bc9c7396dc6c7
Sugrish V M <153385658+sug2004@users.noreply.github.com>
2024-12-15 12:32:15 +0530

    Removed Instagram

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import logo from "../assets/S1.png"
 import { FaLinkedin } from "react-icons/fa"
 import { FaGithub } from "react-icons/fa"
-import { FaInstagram } from "react-icons/fa"
+// import { FaInstagram } from "react-icons/fa"
 import { ImProfile } from "react-icons/im";
 
 import resumePdf from "../assets/Resume/2SUGRISH V M WEB DEVELOPER ML ENGINNER .pdf"
@@ -20,7 +20,7 @@ function Navbar() {
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
             <a className="hover:bg-gradient-to-r hover:from-pink-300 hover:via-slate-500 hover:to-purple-500 text-white font-bold rounded " href="https://www.linkedin.com/in/sugrish-v-m-2793702b3/"><FaLinkedin /></a>
             <a className="hover:bg-gradient-to-r hover:from-pink-300 hover:via-slate-500 hover:to-purple-500 text-white font-bold rounded " href="https://github.com/sug2004"><FaGithub/></a>
-            <a className="hover:bg-gradient-to-r hover:from-pink-300 hover:via-slate-500 hover:to-purple-500 text-white font-bold rounded" href="https://www.instagram.com/sugrishvm/"><FaInstagram/></a>
+            {/* <a className="hover:bg-gradient-to-r hover:from-pink-300 hover:via-slate-500 hover:to-purple-500 text-white font-bold rounded" href="https://www.instagram.com/sugrishvm/"><FaInstagram/></a> */}
             <button className="hover:bg-gradient-to-r hover:from-pink-300 hover:via-slate-500 hover:to-purple-500 text-white font-bold rounded" onClick={handleDownloadResume}>
                <ImProfile />
             </button>
