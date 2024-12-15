adf4223b8859aeb403b331d6d4293186b5ad3573
Sugrish V M <153385658+sug2004@users.noreply.github.com>
2024-12-15 07:10:54 +0530

    About title change

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,10 +6,10 @@ import { motion } from "framer-motion";
 function About() {
   return (
     <div className="border-b border-neutral-900 pb-4">
-    <h1 className="my-20 text-center text-4xl">
+    <motion.h1 whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -100 }} transition={{ duration: 1.5 }} className="my-20 text-center text-4xl">
       About
       <span className="text-neutral-500"> Me</span>
-    </h1>
+    </motion.h1>
     <div className="flex flex-wrap">
       <motion.div whileInView={{opacity:1,x:0}} initial={{opacity:0,x:-100}} transition={{duration:0.5}}  className="w-full lg:w-1/2 lg:p-8">
         <div className="flex items-center justify-center">
